eeddbeff62cc152c23b560ca1e329a2ba0e80416
zhuweitung <zhuweitung@gmail.com>
2021-04-23 15:11:50 +0800

    优化TF-IDF算法

--- src/main/java/com/qianxinyao/analysis/jieba/keyword/TFIDFAnalyzer.java
+++ src/main/java/com/qianxinyao/analysis/jieba/keyword/TFIDFAnalyzer.java
@@ -21,11 +21,11 @@ import com.huaban.analysis.jieba.JiebaSegmenter;
  */
 public class TFIDFAnalyzer
 {
-	
+
 	static HashMap<String,Double> idfMap;
 	static HashSet<String> stopWordsSet;
 	static double idfMedian;
-	
+
 	/**
 	 * tfidf分析方法
 	 * @param content 需要分析的文本/文档内容
@@ -34,16 +34,23 @@ public class TFIDFAnalyzer
 	 */
 	public List<Keyword> analyze(String content,int topN){
 		List<Keyword> keywordList=new ArrayList<>();
-		
+
 		if(stopWordsSet==null) {
-			stopWordsSet=new HashSet<>();
-			loadStopWords(stopWordsSet, this.getClass().getResourceAsStream("/stop_words.txt"));
-		}
+			synchronized (TFIDFAnalyzer.class) {
+				if (stopWordsSet == null) {
+					stopWordsSet = new HashSet<>();
+					loadStopWords(stopWordsSet, this.getClass().getResourceAsStream("/stop_words.txt"));
+				}
+			}		}
 		if(idfMap==null) {
-			idfMap=new HashMap<>();
-			loadIDFMap(idfMap, this.getClass().getResourceAsStream("/idf_dict.txt"));
+			synchronized (TFIDFAnalyzer.class) {
+				if (idfMap == null) {
+					idfMap = new HashMap<>();
+					loadIDFMap(idfMap, this.getClass().getResourceAsStream("/idf_dict.txt"));
+				}
+			}
 		}
-		
+
 		Map<String, Double> tfMap=getTF(content);
 		for(String word:tfMap.keySet()) {
 			// 若该词不在idf文档中，则使用平均的idf值(可能定期需要对新出现的网络词语进行纳入)
@@ -52,18 +59,12 @@ public class TFIDFAnalyzer
 			}else
 				keywordList.add(new Keyword(word,idfMedian*tfMap.get(word)));
 		}
-		
+
 		Collections.sort(keywordList);
-		
-		if(keywordList.size()>topN) {
-			int num=keywordList.size()-topN;
-			for(int i=0;i<num;i++) {
-				keywordList.remove(topN);
-			}
-		}
-		return keywordList;
+
+		return keywordList.size() > topN ? keywordList.subList(0, topN) : keywordList;
 	}
-	
+
 	/**
 	 * tf值计算公式
 	 * tf=N(i,j)/(sum(N(k,j) for all k))
@@ -74,12 +75,12 @@ public class TFIDFAnalyzer
 	private Map<String, Double> getTF(String content) {
 		Map<String,Double> tfMap=new HashMap<>();
 		if(content==null || content.equals(""))
-			return tfMap; 
-		
+			return tfMap;
+
 		JiebaSegmenter segmenter = new JiebaSegmenter();
 		List<String> segments=segmenter.sentenceProcess(content);
 		Map<String,Integer> freqMap=new HashMap<>();
-		
+
 		int wordSum=0;
 		for(String segment:segments) {
 			//停用词不予考虑，单字词不予考虑
@@ -92,15 +93,15 @@ public class TFIDFAnalyzer
 				}
 			}
 		}
-		
+
 		// 计算double型的tf值
 		for(String word:freqMap.keySet()) {
 			tfMap.put(word,freqMap.get(word)*0.1/wordSum);
 		}
-		
-		return tfMap; 
+
+		return tfMap;
 	}
-	
+
 	/**
 	 * 默认jieba分词的停词表
 	 * url:https://github.com/yanyiwu/nodejieba/blob/master/dict/stop_words.utf8
@@ -130,7 +131,7 @@ public class TFIDFAnalyzer
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * idf值本来需要语料库来自己按照公式进行计算，不过jieba分词已经提供了一份很好的idf字典，所以默认直接使用jieba分词的idf字典
 	 * url:https://raw.githubusercontent.com/yanyiwu/nodejieba/master/dict/idf.utf8
@@ -155,7 +156,7 @@ public class TFIDFAnalyzer
 			{
 				e.printStackTrace();
 			}
-			
+
 			// 计算idf值的中位数
 			List<Double> idfList=new ArrayList<>(map.values());
 			Collections.sort(idfList);
@@ -166,7 +167,7 @@ public class TFIDFAnalyzer
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args)
 	{
 		String content="孩子上了幼儿园 安全防拐教育要做好";
